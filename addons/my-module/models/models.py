494fa24a7113a123ddbc9adac349f2d9eed43632
Matheus Tanaques <matheus.tanaques@polibrasnet.com.br>
2017-08-22 09:38:37 -0300

    [CHG] Basic views do doc 10.0 odoo

--- addons/my-module/models/models.py
+++ addons/my-module/models/models.py
@@ -2,14 +2,9 @@
 
 from odoo import models, fields, api
 
-# class my-module(models.Model):
-#     _name = 'my-module.my-module'
+class Mymodule(models.Model):
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    _name = 'academy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
